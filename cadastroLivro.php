00caf23ab7f7a9d20a095dcee6dee43b35b540f8
Hendy <hendy.almeida@hotmail.com>
2020-02-05 12:31:02 -0300

    aula 1 pdo - create e read pronto

--- cadastroLivro.php
+++ cadastroLivro.php
@@ -1,37 +1,65 @@
-<?php 
+<?php
 require_once('conexao.php');
 $consulta = $conexaoDB->prepare('SELECT * from editora');
 $resultado = $consulta->execute();
 
 $editoras = $consulta->fetchAll(PDO::FETCH_ASSOC);
+
+if (isset($_POST['cadastro-livro'])) {
+
+  //verifica campos preenchidos
+  if ($_POST['nome'] != "" && $_POST['descricao'] != "") {
+
+    //prepara a query
+    $query = $conexaoDB->prepare('INSERT INTO produto (nome, descricao, preco, fk_editora, fk_categoria, fk_autor, imagem) VALUES (:nome, :descricao, :preco, :fk_editora, 49, 41, "sem-imagem")');
+
+    $resultado = $query->execute([
+      ":nome" => $_POST['nome'],
+      ":descricao" => $_POST['descricao'],
+      ":preco" => $_POST['preco'],
+      ":fk_editora" => $_POST['fk_editora']
+    ]);
+
+    //se tudo der certo, redireciona para lista de livros
+    header('location: livros.php');
+  }
+}
+
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
   <title>Cadastro</title>
 </head>
+
 <body>
-  <form action="" method="POST">
-    <label for="nomeProduto">Nome Produto:</label>
-        <input type="text" id='nomeProduto'>
+  <div class="container my-5">
+    <h1>Cadastrar livro</h1>
+  </div>
+  <form action="" method="POST" class="container">
+    <label for="nome">Nome Produto:</label>
+    <input type="text" id="nome" name="nome" class="form-control">
     <label for="descricao">Descrição:</label>
-        <input type="text" name="" id="descricao">
-    <label for="">Preço:</label>
-         <input type="number">
-    <label for="">Imagem:</label>
-        <input type="file">
-     <label for="">Editora</label>
-         <select name="" id="">
-        <?php foreach($editoras as $editora) { ?>
-            <option value=""> <?php echo $editora["nome"]; ?></option>
-        <?php } ?>
-         </select>
-         <button> Enviar </button>
-
-
+    <input type="text" name="descricao" id="descricao" class="form-control">
+    <label for="preco">Preço:</label>
+    <input type="number" name="preco" id="preco" class="form-control">
+    <label for="imagem">Imagem:</label>
+    <input type="file" name="imagem" id="imagem" class="form-control">
+    <label for="fk_editora">Editora</label>
+    <select name="fk_editora" id="fk_editora" class="form-control">
+      <?php foreach ($editoras as $editora) { ?>
+        <option value="<?php echo $editora["id"]; ?>"> <?php echo $editora["nome"]; ?></option>
+      <?php } ?>
+    </select>
+    <button name="cadastro-livro" class="btn btn-primary"> Enviar </button>
   </form>
 </body>
+
 </html>
--- livros.php
+++ livros.php
@@ -1,12 +1,62 @@
-<?php 
-require_once ('conexao.php');
+<?php
+require_once('conexao.php');
 $consultaDB = $conexaoDB->prepare('SELECT * from produto');
 $consulta = $consultaDB->execute();
 
 $livros = $consultaDB->fetchAll(PDO::FETCH_ASSOC);
 
-foreach($livros as $livro){
- 
-  echo $livro["nome"]."<br>".$livro["descricao"]."<br>".$livro["preco"]."<hr>";
-}
+// foreach ($livros as $livro) {
+
+//   echo $livro["nome"] . "<br>" . $livro["descricao"] . "<br>" . $livro["preco"] . "<hr>";
+// }
 ?>
+
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <title>Livraria</title>
+</head>
+
+<body>
+  <div class="container my-5">
+    <div class="row">
+      <div class="col">
+        <h1>Livraria</h1>
+      </div>
+      <div class="col text-right">
+        <a href="cadastroLivro.php" class="btn btn-primary">Cadastrar livro</a>
+      </div>
+    </div>
+  </div>
+  <table class="table container">
+    <thead>
+      <tr>
+        <th>Nome</th>
+        <th>Descrição</th>
+        <th>Preço</th>
+        <th>Categoria</th>
+        <th>Editora</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php foreach ($livros as $livro) : ?>
+        <tr>
+          <td><?php echo $livro['nome'] ?></td>
+          <td><?php echo $livro['descricao'] ?></td>
+          <td><?php echo $livro['preco'] ?></td>
+          <td><?php echo $livro['fk_categoria'] ?></td>
+          <td><?php echo $livro['fk_editora'] ?></td>
+        </tr>
+      <?php endforeach; ?>
+    </tbody>
+  </table>
+</body>
+
+</html>
